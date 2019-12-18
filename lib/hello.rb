22b345aba03588a0cb817fffe151bc84f5e99285
Rick Lim <rick.jason.a.lim@gmail.com>
2019-12-18 11:59:26 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,9 +1,9 @@
 def hello_t(array)
-  array.each do |name|
-    if name.start_with?("T")
-      puts "Hi, #{name}"
-    end
-end
+  i=0
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
 end
 # call your method here!
 
